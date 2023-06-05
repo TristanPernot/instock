25451b88474e2b62ae76e2696cdb5cf80c110b69
Tristan Pernot <pernottristan@gmail.com>
2023-06-05 16:08:58 -0700

    DO NOT MERGE just small fixes to files

---
+++ src/components/WarehouseList/WarehouseList.js
@@ -0,0 +1,13 @@
+function warehouseList() {
+    return(
+        <div className="container">
+            <ul className="container__list">
+                <li>
+                    
+                </li>
+            </ul>
+        </div>
+
+    )
+}
+export default warehouseList()
