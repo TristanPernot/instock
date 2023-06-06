705c69c11d5c1e4b4ddaee39325b3b61aa4984ed
ericayagi <eyagi01@gmail.com>
2023-06-06 09:05:39 -0700

    add initial styling

--- src/components/Inventory/Inventory.jsx
+++ src/components/Inventory/Inventory.jsx
@@ -1,9 +1,11 @@
 import "../Inventory/Inventory.scss";
+import deleteIcon from "../../assets/Icons/delete_outline-24px.svg";
+import editIcon from "../../assets/Icons/edit-24px.svg";
 
 function Inventory() {
   return (
-    <>
-      <div className="inventory">
+    <div className="inventory">
+      <div className="inventory__container">
         <div className="inventory__searchContainer">
           <h1 className="inventory__header">Inventory</h1>
           <input
@@ -14,39 +16,30 @@ function Inventory() {
           <button className="inventory__button">+Add New Item</button>
         </div>
         <div className="inventory__itemContainer">
-          <div className="inventory__mobileContainer1"></div>
-          <div className="inventory__tableHeader">INVENTORY ITEM</div>
-          <div className="inventory__item">{}</div>
-          <div className="inventory__tableHeader">CATEGORY</div>
-          <div className="inventory__body">{}</div>
-
-          <div className="inventory__mobileContainer2">
-            <div className="inventory__tableHeader">STATUS</div>
-            <div className="inventory__body">{}</div>
-            <div className="inventory__tableHeader">QTY</div>
-            <div className="inventory__body">{}</div>
-            <div className="inventory__tableHeader">WAREHOUSE</div>
-            <div className="inventory__body">{}</div>
+          <div className="inventory__mobileContainer1">
+            <div className="inventory__mobileContainer2">
+              <div className="inventory__tableHeader">INVENTORY ITEM</div>
+              <div className="inventory__item">{}</div>
+              <div className="inventory__tableHeader">CATEGORY</div>
+              <div className="inventory__body">{}</div>
+            </div>
+            <div className="inventory__mobileContainer3">
+              <div className="inventory__tableHeader">STATUS</div>
+              <div className="inventory__body">{}</div>
+              <div className="inventory__tableHeader">QTY</div>
+              <div className="inventory__body">{}</div>
+              <div className="inventory__tableHeader">WAREHOUSE</div>
+              <div className="inventory__body">{}</div>
+            </div>
           </div>
-
           <div className="inventory__iconContainer">
-            <div>
-              <button>
-                <img
-                  src="../../assets/Icons/delete_outline-24px.svg"
-                  alt="delete"
-                />
-              </button>
-            </div>
-            <div>
-              <button>
-                <img src="../../assets/Icons/edit-24px.svg" alt="edit" />
-              </button>
-            </div>
+            <img src={deleteIcon} alt="delete" />
+
+            <img src={editIcon} alt="edit" />
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
