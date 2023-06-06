4ba40ea9cef9aa500bce76fb810bb36f83e57c4d
Tristan Pernot <pernottristan@gmail.com>
2023-06-06 09:09:15 -0700

    commiting to pull not finished

--- src/components/WarehouseList/WarehouseList.jsx
+++ src/components/WarehouseList/WarehouseList.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import arrowright from '../../assets/Icons/chevron_right-24px.svg'
 import './WarehouseList.scss'
 function warehouseList() {
@@ -9,6 +10,7 @@ function warehouseList() {
                 </li>
                 <li className='container__lis'>
                     <p>Test</p>
+                    <p>Second test</p>
                 </li>
                 <li>
                     <p>Test</p>
