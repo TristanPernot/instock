3afc5fe8e528b42d7d149a2c753af37a35ce7d91
Tristan Pernot <pernottristan@gmail.com>
2023-06-07 12:34:10 -0700

    working still on the scss

--- src/components/WarehouseList/WarehouseList.jsx
+++ src/components/WarehouseList/WarehouseList.jsx
@@ -1,57 +1,82 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import arrowright from '../../assets/Icons/chevron_right-24px.svg'
 import sort from '../../assets/Icons/sort-24px.svg'
 import './WarehouseList.scss'
 import deleteimg from '../../assets/Icons/delete_outline-24px.svg'
 import editing from '../../assets/Icons/edit-24px.svg'
-
 function warehouseList() {
+    const api = process.env.REACT_APP_API_URL;
+
 
     return(
+        
         <div className="container">
+            <h1>Warehouse</h1>
+            <form className='container__form'>
+            <input
+                  id="search"
+                  type="text"
+                  className='container__searchbox'
+                  placeholder="Search..."
+                  name="searchBar"
+                />
+            </form>
             <ul className='container__headerlist'>
-                <li className='container__li'>
+                <li className='container__li--header'>
                 <span className='container__li--item'>
-                    <p>WAREHOUSE</p> 
+                    <h4>WAREHOUSE</h4> 
                     <img src={`${sort}`} alt="sort" />
                     </span>
                     <span className='container__li--item'>
-                    <p>ADRESS</p> 
+                    <h4>ADRESS</h4> 
                     <img src={`${sort}`} alt="sort" />
                     </span>
                     <span className='container__li--item'>
-                    <p>CONTACT NAME</p> 
+                    <h4>CONTACT NAME</h4> 
                     <img src={`${sort}`} alt="sort" />
                     </span>
                     <span className='container__li--item'>
-                    <p>CONTACT INFORMATION</p> 
+                    <h4>CONTACT INFORMATION</h4> 
                     <img src={`${sort}`} alt="sort" />
                     </span>
                     <span className='container__li--item'>
-                    <p>ACTIONS</p> 
+                    <h4>ACTIONS</h4> 
                     <img src={`${sort}`} alt="sort" />
                     </span>
                 </li>
             </ul>
             <ul className="container__list">
-                <li className='container__li'>
-                    <p>Test</p>
-                </li>
                 <li className='container__li hoverdiv'>
-                    <span className='container__li--item'>
-                    <p className='container__li--special'>Test</p> 
-                    <img src={`${arrowright}`} alt="arrow" />
-                    </span>
-                    <p className='container__li--item'>Second test</p>
-                    <p className='container__li--item'>Third test</p>
-                    <p className='container__li--item'>Fourth test</p>
+                    <div className='container__li--format'>
+                        <span className='container__li--mobile'>
+                            <h4>Warehouse</h4> 
+                        </span>
+                        <span className='container__li--item'>
+                            <p className='container__li--special'>Test</p> 
+                            <img src={`${arrowright}`} alt="arrow" />
+                        </span>
+                        <span className='container__li--mobile'>
+                            <h4>Adress</h4> 
+                        </span>
+                        <p className='container__li--item'>Second test</p>
+                    </div>
+                    <div className="container__li--format">
+                        <span className='container__li--mobile'>
+                            <h4>Contact Name</h4> 
+                        </span>
+                        <p className='container__li--item'>Third test</p>
+                        <span className='container__li--mobile'>
+                            <h4>Contact information</h4> 
+                        </span>
+                        <p className='container__li--item'>Fourth test</p>
+                    </div>
                     <span className='container__li--img'>
-                    <img src={`${deleteimg}`} alt="delete" />
-                    <img src={`${editing}`} alt="edit" />
+                        <img src={`${deleteimg}`} alt="delete" />
+                        <img src={`${editing}`} alt="edit" />
                     </span>
-                </li>
-                <li className='container__li'>
-                    <p>Test</p>
                 </li>
             </ul>
         </div>
