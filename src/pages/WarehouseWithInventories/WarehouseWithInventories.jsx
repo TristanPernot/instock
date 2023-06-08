498e5e5d8eb4581f647e668e7dfcd4714561bf0e
Neat <sophaneat@student.fdu.edu>
2023-06-07 20:52:06 -0700

    Add styles for responsiveness for each breakpoint

--- src/pages/WarehouseWithInventories/WarehouseWithInventories.jsx
+++ src/pages/WarehouseWithInventories/WarehouseWithInventories.jsx
@@ -2,21 +2,28 @@ import "./WarehouseWithInventories.scss";
 import BackIcon from "../../assets/Icons/arrow_back-24px.svg";
 import EditIcon from "../../assets/Icons/edit-white-24px.svg";
 import BlueEditIcon from "../../assets/Icons/edit-24px.svg";
-import DeleteIcon from "../../assets/Icons/delete_outline-24px.svg"
+import DeleteIcon from "../../assets/Icons/delete_outline-24px.svg";
+import MenuIcon from "../../assets/Icons/sort-24px.svg";
+import FowardIcon from "../../assets/Icons/chevron_right-24px.svg";
 import {useParams} from "react-router-dom";
 import {useState, useEffect} from "react";
 import axios from "axios";
 const WarehouseWithInventories = () => {
     const {id} = useParams();
     const [warehouse, setWarehouse] = useState({});
-
+    const [inventories, setInventories] = useState([]);
     // Fetch info about the warehouse
     useEffect(()=> {
         axios.get(`http://localhost:8080/warehouse/${id}`)
             .then((response) => setWarehouse(response.data))
             .catch((error) => console.log(error))
     }, [])
-
+    // Fetch all inventory for a given warehouse
+    useEffect(() => {
+        axios.get(`http://localhost:8080/inventory/forWarehouse/${id}`)
+            .then((response) => setInventories(response.data))
+            .catch((error) => console.log(error))
+    }, [])
     return (
         <div className="warehouseInventories">
             {/* Header */}
@@ -53,44 +60,81 @@ const WarehouseWithInventories = () => {
             }
             {/* Inventories */}
             <div className="inventories__content">
-                {/* Item */}
-                <div className="inventory__item">
+                {/* Header */}
+                <div className="inventories__header">
                     {/* One */}
                     <div className="inventory__left">
-                        <div className="col col--50">
-                            <div className="col__title">INVENTORY ITEM</div>
-                            <div className="col__info">Television</div>
+                        <div className="col w-50">
+                            <p className="col__title-header">INVENTORY ITEM</p>
+                            <img className="col__btn" src={MenuIcon} alt="Menu Icon"/>
                         </div>
                         <div className="col">
-                            <div className="col__title">CATEGORY</div>
-                            <div className="col__info">Electronics</div>
+                            <p className="col__title-header">CATEGORY</p>
+                            <img className="col__btn" src={MenuIcon} alt="Menu Icon"/>
                         </div>
                     </div>
                     {/* Two */}
                     <div className="inventory__right">
-                        <div className="col col--30">
-                            <div className="col__title">STATUS</div>
-                            <div className="col__info">IN STOCK</div>
+                        <div className="col w-50">
+                            <p className="col__title-header">STATUS</p>
+                            <img className="col__btn" src={MenuIcon} alt="Menu Icon"/>
                         </div>
-                        <div className="col col--30">
-                            <div className="col__title">QTY</div>
-                            <div className="col__info">500</div>
-                        </div>
-                        <div className="col col--30">
-                            <div className="col__title">WAREHOUSE</div>
-                            <div className="col__info">Manhattan</div>
+                        <div className="col">
+                            <p className="col__title-header">QTY</p>
+                            <img className="col__btn" src={MenuIcon} alt="Menu Icon"/>
                         </div>
                     </div>
                     {/* Three */}
                     <div className="inventory__bottom">
-                        <button className="inventory__btn">
-                            <img className="inventory__deleteBtn" src={DeleteIcon} alt="Delete Icon"/>
-                        </button>
-                        <button className="inventory__btn">
-                            <img src={BlueEditIcon} alt="Edit Icon"/>
-                        </button>
+                        <div className="col">
+                            <p className="col__title-header">ACTION</p>
+                        </div>
                     </div>
                 </div>
+
+                {/* Item */}
+                {
+                    inventories.map((inventory) => {
+                        return (
+                                <div key={inventory.id} className="inventory__item">
+                                {/* One */}
+                                <div className="inventory__left">
+                                    <div className="col w-50">
+                                        <p className="col__title">INVENTORY ITEM</p>
+                                        <div className="col__info col__inventory-name">
+                                            <p>{inventory.item_name}</p>
+                                            <img className="col__inventory-name__btn" src={FowardIcon} alt="Menu Icon"/>
+                                        </div>
+                                    </div>
+                                    <div className="col">
+                                        <p className="col__title">CATEGORY</p>
+                                        <p className="col__info">{inventory.category}</p>
+                                    </div>
+                                </div>
+                                {/* Two */}
+                                <div className="inventory__right">
+                                    <div className="col w-50">
+                                        <p className="col__title">STATUS</p>
+                                        <p className="col__info">{inventory.status}</p>
+                                    </div>
+                                    <div className="col">
+                                        <p className="col__title">QTY</p>
+                                        <p className="col__info">{inventory.quantity}</p>
+                                    </div>
+                                </div>
+                                {/* Three */}
+                                <div className="inventory__bottom">
+                                    <button className="inventory__btn">
+                                        <img className="inventory__deleteBtn" src={DeleteIcon} alt="Delete Icon"/>
+                                    </button>
+                                    <button className="inventory__btn">
+                                        <img src={BlueEditIcon} alt="Edit Icon"/>
+                                    </button>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
             </div>
         </div>
     )
